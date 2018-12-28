5609683484a5f6a6d71ea1fd80dc42cea595fb20
Muhammad Nazam <muhammadnazam@Muhammads-MBP.attlocal.net>
2018-12-27 23:02:04 -0800

    adding profile page

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const mongoose = require ('mongoose');
+const { check, validationResult } = require('express-validator/check');
+
+const Profile = require ('../../models/Profile');
+const User = require('../../models/User');
 
 //@route    Get api/profile/test
 //@desc     Tests Profile Route
 //@access   Public
 router.get('/test', (req, res) => res.json({msg: 'Profile Works'}));
 
+router.get('/', passport.authenticate('jwt', {session : false }), (req, res) => {
+
+    Profile.findOne({ user : req.user.id })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile){
+                return res.status(404).json({noprofile : 'there is no profile'});
+            }else {
+                return res.json(profile);
+            } 
+        })
+});
+
+router.post('/', [check('handle', 'Must Be provide a handle Name').isEmpty()],passport.authenticate('jwt', {session : false }), (req, res) => {
+
+    
+
+    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+    const profileFields = {};
+    profileFields.user = req.user.id
+
+    if(req.body.handle) profileFields.handle = req.body.handle;
+    if(req.body.address) profileFields.address = req.body.address;
+
+    Profile.findOne({ user : req.user.id})
+        .then(profile => {
+            if(profile) {
+                Profile.findOneAndUpdate({ user : req.user.id}, { $set : profileFields }, {new : true})
+                    .then(profile => res.json(profile));
+            } else {
+                Profile.findOne({ handle : profileFields.handle})
+                .populate('user', ['name', 'avatar'])
+                .then (profile => {
+                    if(profile){
+                        errors.handle = "That Handle already Exist";
+                        res.status(400).json(errors);
+                    }
+                    //Save Profile
+
+                    new Profile(profileFields).save().then(profile => res.json(profile));
+                })
+            }
+        });
+});
 
 module.exports= router;
