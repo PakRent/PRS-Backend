19ce0c24db6d71d5a24e6a6a58d5b102c25454e4
Muhammad Nazam <muhammadnazam@Muhammads-MBP.attlocal.net>
2018-12-30 00:56:19 -0800

    add delete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage, fileFilter: fileFilter});
 
 
-  
+const Profile = require('../../models/Profile');
 
 const Post = require('../../models/Posts');
 
@@ -34,6 +34,38 @@ const Post = require('../../models/Posts');
 //@desc     Tests Post Route
 //@access   Public
 router.get('/test', (req, res) => res.json({msg: 'Posts Works'}));
+
+//@route    Get api/posts
+//@desc     Get Posts
+//@access   Public
+router.get('/', (req, res) => {
+    Post.find()
+        .sort(
+            {
+                date: -1
+            }
+        )
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({nopostfound: 'No Post Found'}));
+});
+
+//@route    Get api/posts
+//@desc     Get Single Post
+//@access   Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({nopostfound: 'No Post Found With this id'}));
+});
+
+//@route    Get api/posts
+//@desc     Get Single Post
+//@access   Public
+router.get('/:name', passport.authenticate('jwt', {session:false}), (req, res) => {
+    Post.find()
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({nopostfound: 'No Post Found With this id'}));
+});
 
 //@route    Post api/posts
 //@desc     Create Post Route
@@ -66,5 +98,19 @@ router.post("/", upload.single('image'), passport.authenticate('jwt', {session:f
       });
   });
   
+
+router.delete('/:id', passport.authenticate('jwt', {session:false}), (req, res) => {
+    Profile.findOne({user : req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+            .then(post =>{
+                if(post.user.toString() !== req.user.id) {
+                    return res.status(401).json({notauthorized : "user not authorized"});
+                }
+                post.remove().then(() =>{res.json({success: true})});
+            })
+        })
+
+});
 
 module.exports= router;
