42e02738e79900af634645c02667774005912824
Muhammad Nazam <muhammadnazam@Muhammads-MacBook-Pro.local>
2019-01-19 16:39:35 -0800

    change password

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     //mongoURI : 'mongodb://knazim667:Salman007@ds155263.mlab.com:55263/rentit',
-    mongoURI : "mongodb://knazim667:Salman007;'@ds151127.mlab.com:51127/heroku_1s26p8m4",
+    mongoURI : "mongodb://knazim667:Salman007@ds151127.mlab.com:51127/heroku_1s26p8m4",
     secretOrKey : 'secret'
 }
